eb22d26aff3c8b22116fdf90a8865657e93670d4
ember169 <182413924+ember169@users.noreply.github.com>
2025-12-20 09:48:21 +0100

    init

---
+++ includes/minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgervet <42@leogervet.com>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/20 09:40:28 by lgervet           #+#    #+#             */
+/*   Updated: 2025/12/20 09:44:41 by lgervet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include "libft.a"
+
+#endif
