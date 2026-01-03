c2e60294ecd033b9236b4c5ccd6a6134f16fa62f
Leo Gervet <lgervet@f1r2s3.clusters.42paris.fr>
2026-01-03 16:15:17 +0100

    progress made on encoding

--- tests/test.c
+++ tests/test.c
@@ -6,7 +6,7 @@
 /*   By: lgervet <42@leogervet.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 14:52:00 by lgervet           #+#    #+#             */
-/*   Updated: 2025/12/30 16:33:07 by lgervet          ###   ########.fr       */
+/*   Updated: 2026/01/03 16:14:54 by lgervet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,50 +23,108 @@
 **     @param n  bytes needed for character
 **     @return binary string
 */
-void single_char_converter(char c, int n)
+// char *ascii_char_to_bin(char c, int bytes_nb)
+// {
+// 	char	*dst;
+// 	int		decimal;
+// 	int		i;
+
+// 	dst = (char *)malloc((bytes_nb + 1) * sizeof(char));
+// 	if (!dst)
+// 		return (NULL);
+// 	decimal = (int)c;
+// 	i = bytes_nb - 1;
+// 	while (i)
+// 	{
+// 		dst[i] = ft_itoa(decimal % 2);
+// 		decimal /= 2;
+// 		i--;
+// 	}
+// 	dst[n] = '\0';
+// 	printf("[ ] char %c (%d) = %s\n", c, (int)c, dst);
+// 	return (dst);
+// }
+
+// char	*ascii_str_to_bin(char	*s)
+// {
+// 	int		i;
+// 	int		bytes_nb;
+// 	char	*dst;
+
+// 	bytes_nb = 8;
+// 	dst = malloc((strlen(s) * sizeof(char)) * bytes_nb);
+// 	if (!dst)
+// 		return (NULL);
+// 	i = 0;
+// 	while (s[i])
+// 	{
+// 		dst[i] = ascii_char_to_bin(s[i], bytes_nb);
+// 		i++;
+// 	}
+// 	return (dst);
+// }
+
+// int	main(void)
+// {
+// 	char	*src = "lorem";
+// 	char	*dst[5][8];
+// 	char	*end;
+
+// 	printf("[ ] src = %s\n", src);
+// 	printf("[X] CONVERTING...\n");
+// 	ascii_str_to_bin(src);
+// 	printf("[X] CONVERTING BACK...\n");
+// 	// bin_converter(dst);
+// 	return (0);
+// }
+
+// printf("[ ] char %c (%d) = %d%d%d%d%d%d%d%d\n", c, (int)c, dst[0], dst[1], dst[2], dst[3], dst[4], dst[5], dst[6], dst[7]);
+
+/*
+	binary utf-8 character = 8 bytes
+	  -> 1 char =  1 str
+	one str in binary = multiple str (1 by char)
+*/
+
+char	*convert_char_to_binary(int char_int, int byte_nb)
 {
-	char	*dst;
-	int		decimal;
-	int		i;
+	int		i = 0;
+	int		to_convert = char_int;
+	char	*encoded_char;
 
-	dst = (char *)malloc((n + 1) * sizeof(char));
-	if (!dst)
-		return ;
-	decimal = (int)c;
-	i = n - 1;
-	while (i)
+	encoded_char = malloc((8 * sizeof(char)) + 1);
+	if (!encoded_char)
+		return (NULL);
+	while (i < byte_nb)
 	{
-		dst[i] = ft_itoa(decimal % 2);
-		decimal /= 2;
-		i--;
-	}
-	dst[n] = '\0';
-	// printf("[ ] char %c (%d) = %d%d%d%d%d%d%d%d\n", c, (int)c, dst[0], dst[1], dst[2], dst[3], dst[4], dst[5], dst[6], dst[7]);
-	printf("[ ] char %c (%d) = %s\n", c, (int)c, dst);
-}
-
-void	string_converter(char	*s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		single_char_converter(s[i], 8);
+		encoded_char[i] = (to_convert % 2) + '0';
+		to_convert /= 2;
 		i++;
 	}
+	encoded_char[i] = '\0';
+	printf("[ ] Encoded char %d: %s\n", char_int, encoded_char);
+	return (encoded_char);
 }
 
-int	main(void)
+int main()
 {
-	char	*src = "lorem";
-	char	*dst[5][8];
-	char	*end;
+	int		byte_nb = 8 ; // for utf8
+	int 	i = 0;
+	int 	len = 0;
+	char	*input = "lorem";
+	char 	**encoded;
 
-	printf("[ ] src = %s\n", src);
-	printf("[X] CONVERTING...\n");
-	string_converter(src);
-	printf("[X] CONVERTING BACK...\n");
-	// bin_converter(dst);
-	return (0);
+	len = strlen(input);
+	encoded = (char *)malloc((sizeof(char) * (len + 1)) * 8);
+	if (!encoded)
+		return (NULL);
+	printf("[x] IN: %s\n", input);
+	while (i < len)
+	{
+		encoded[i] = convert_char_to_binary((int)input[i], byte_nb);
+		i++;
+	}
+	printf("[x] OUT: ");
+	for (i = 0; i < len; i++)
+		printf("%s ", encoded[i]);
 }
